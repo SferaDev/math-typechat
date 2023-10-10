da26906df57cdc742a140ba67dd905fe9c7b49e7
Alexis Rico <me@sferadev.com>
2023-10-10 16:55:58 -0700

    Update math.ts

--- math.ts
+++ math.ts
@@ -1,28 +1,29 @@
-// This is a schema for writing programs that evaluate expressions.
+// Add two numbers
+export function add(x: number, y: number): number {
+  return x + y;
+}
 
-export const API = {
-  // Add two numbers
-  add(x: number, y: number): number {
-    return x + y;
-  },
-  // Subtract two numbers
-  sub(x: number, y: number): number {
-    return x - y;
-  },
-  // Multiply two numbers
-  mul(x: number, y: number): number {
-    return x * y;
-  },
-  // Divide two numbers
-  div(x: number, y: number): number {
-    return x / y;
-  },
-  // Negate a number
-  neg(x: number): number {
-    return -x;
-  },
-  // Identity function
-  id(x: number): number {
-    return x;
-  },
-};
+// Subtract two numbers
+export function sub(x: number, y: number): number {
+  return x - y;
+}
+
+// Multiply two numbers
+export function mul(x: number, y: number): number {
+  return x * y;
+}
+
+// Divide two numbers
+export function div(x: number, y: number): number {
+  return x / y;
+}
+
+// Negate a number
+export function neg(x: number): number {
+  return -x;
+}
+
+// Identity function
+export function id(x: number): number {
+  return x;
+}
